557324375004ddd9e7e4f98f5ab31655c3ed6861
majid0334 <113986626+majid0334@users.noreply.github.com>
2023-03-06 19:41:18 +0100

    Read me changes and js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # react-todoLost-Firebase
+Majid Moradi
--- hacker-stories/src/db/firebase.js
+++ hacker-stories/src/db/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0HZd4kOtzcpyGffv6EM6gsmTicObAoe8",
-  authDomain: "react-bc3a4.firebaseapp.com",
-  projectId: "react-bc3a4",
-  storageBucket: "react-bc3a4.appspot.com",
-  messagingSenderId: "929742958683",
-  appId: "1:929742958683:web:a825574e8da32641da4f7e",
-  measurementId: "G-B85SBL5JCW",
+  //skriva din eget
 };
 
 const app = initializeApp(firebaseConfig);
